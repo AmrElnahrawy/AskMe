483ef5c522b70ca8b3fa2693981e05ad6418b564
Amr Elnahrawy <amrelnahrawy05@gmail.com>
2025-08-12 18:29:36 +0300

    implement second menu

--- app.cpp
+++ app.cpp
@@ -18,20 +18,56 @@ struct main_system
         ifstream ud("users_data.txt");
         if (ud.fail())
         {
-            cout << "Sorry, can't connect to the server servers" << endl;
+            cout << "Sorry, can't connect to the server" << endl;
             return 0;
         }
         string user;
         while (getline(ud, user))
         {
             istringstream iss(user);
-            iss >> users_data[ids].id; 
-            iss >> users_data[ids].name; 
+            iss >> users_data[ids].id;
+            iss >> users_data[ids].name;
             iss >> users_data[ids].password;
-            ids++; 
+            ids++;
         }
         ud.close();
         return 1;
+    }
+
+    void run()
+    {
+        int choice{0};
+        int account;
+        load_data();
+        while (true)
+        {
+            account = first_menu();
+            if (account == 0)
+                return;
+            while (true)
+            {
+                choice = second_menu();
+                // if (choice == 1)
+                //     questions_to_me();
+                // else if (choice == 2)
+                //     questions_from_me();
+                // else if (choice == 3)
+                //     answer_question();
+                // else if (choice == 4)
+                //     delete_question();
+                // else if (choice == 5)
+                //     ask_question();
+                // else if (choice == 6)
+                //     list_system_users();
+                // else if (choice == 7)
+                //     feed();
+                /* else */if (choice == 8) {
+                    cout << "***********************************" << endl;
+                    break;
+                }
+                   
+            }
+        }
     }
 
     int first_menu()
@@ -44,30 +80,25 @@ struct main_system
             cout << "\t2) Sign Up" << endl;
             cout << "\t3) Exit" << endl
                  << endl;
-            cout << "Enter choice: ";
+            cout << "Enter number in range (1 - 3): ";
             getline(cin, schoice);
             if (schoice.size() != 1 || !(schoice[0] == '1' || schoice[0] == '2' || schoice[0] == '3'))
             {
-                cout << "Invalid input. Try again" << endl;
+                cout << "ERROR: Invalid input...Try again" << endl;
                 cout << "***********************************" << endl;
                 continue;
             }
             int choice = (int)schoice[0] - '0';
             if (choice == 1)
-            {
                 return login();
-            }
             else if (choice == 2)
-            {
                 return signup();
-            }
             else if (choice == 3)
-            {
                 return 0;
-            }
         }
         return 0;
     }
+
     int login()
     {
         string name, password;
@@ -78,7 +109,7 @@ struct main_system
             int crnt_id{0};
             if (name.find(' ') != string::npos)
             {
-                cout << "Invalid input. Try again" << endl;
+                cout << "ERROR: No space allowed...Try again" << endl;
                 cout << "***********************************" << endl;
                 continue;
             }
@@ -100,7 +131,7 @@ struct main_system
             getline(cin, password);
             if (password.find(' ') != string::npos)
             {
-                cout << "Invalid input. Try again" << endl;
+                cout << "ERROR: Invalid input...Try again" << endl;
                 cout << "***********************************" << endl;
                 continue;
             }
@@ -130,7 +161,7 @@ struct main_system
             getline(cin, name);
             if (name.find(' ') != string::npos)
             {
-                cout << "Invalid input. Try again" << endl;
+                cout << "ERROR: No space allowed...Try again" << endl;
                 cout << "***********************************" << endl;
                 continue;
             }
@@ -150,7 +181,7 @@ struct main_system
             getline(cin, password);
             if (password.find(' ') != string::npos)
             {
-                cout << "Invalid input. Try again" << endl;
+                cout << "ERROR: No space allowed...Try again" << endl;
                 cout << "***********************************" << endl;
                 continue;
             }
@@ -166,10 +197,31 @@ struct main_system
         return ids - 1;
     }
 
-    void run()
+    int second_menu()
     {
-        load_data();
-        first_menu();
+        string schoice;
+        while (true)
+        {
+            cout << "Menu:" << endl;
+            cout << "\t1) Print Questions To Me" << endl;
+            cout << "\t2) Print Questions From Me" << endl;
+            cout << "\t3) Answer Question" << endl;
+            cout << "\t4) Delete Question" << endl;
+            cout << "\t5) Ask Question" << endl;
+            cout << "\t6) List System Users" << endl;
+            cout << "\t7) Feed" << endl;
+            cout << "\t8) Logout" << endl << endl;
+            cout << "Enter number in range (1 - 8): ";
+            getline(cin, schoice);
+            if (schoice.size() != 1 || !(int('1') <= (int)schoice[0] && (int)schoice[0] <= int('8')))
+            {
+                cout << "ERROR: Invalid input...Try again" << endl;
+                cout << "***********************************" << endl;
+                continue;
+            }
+            int choice = (int)schoice[0] - '0';
+            return choice;
+        }
     }
 };
 
@@ -177,5 +229,5 @@ int main()
 {
     main_system AskMe;
     AskMe.run();
-
+    return 0;
 }
